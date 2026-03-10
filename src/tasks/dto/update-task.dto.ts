9aa21f5477a72933cbe7c649864a4bb5a4294bc1
SamuelSotomayor1 <andressotomayor477@gmail.com>
2026-03-10 06:07:17 -0300

    feat: complete task controller with full crud

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDTO } from './create-task.dto';
 
+// El DTO de actualización extiende del DTO de creación con todos los campos son opcionales
 export class UpdateTaskDTO extends PartialType(CreateTaskDTO) {}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { UpdateTaskDTO } from './dto/update-task.dto';
 
-@Controller('tasks')
-export class TasksController {}
+@Controller('tasks') //Ruta base (http://localhost:3000/api/tasks)
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  // Obtener todas las tareas
+  @Get()
+  findAll() {
+    return this.tasksService.getAll();
+  }
+
+  // Crear una tarea
+  @Post()
+  create(@Body() createTaskDTO: CreateTaskDTO) {
+    return this.tasksService.create(createTaskDTO);
+  }
+
+  // Actualizar una tarea
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateTaskDTO: UpdateTaskDTO,
+  ) {
+    return this.tasksService.update(id, updateTaskDTO);
+  }
+
+  // Eliminar una tarea
+  @Delete(':id')
+  delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.tasksService.delete(id);
+  }
+}
